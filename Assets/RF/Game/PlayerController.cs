326fd8bb7f1b4bee2912888f07c452435b8925c0
REFIRSER2 <84903837+REFIRSER2@users.noreply.github.com>
2021-11-16 17:30:21 +0900

    드래그 선택 추가
    드래그 선택 조작이 추가되었습니다.

--- Assets/RF/Game/PlayerController.cs
+++ Assets/RF/Game/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -91,16 +92,56 @@ public class PlayerController : MonoBehaviour
     #endregion
  
     #region 드래그
+
+    [SerializeField] private List<UnitBase> unitList = new List<UnitBase>();
+    [SerializeField] private List<UnitBase> selectedUnits = new List<UnitBase>();
+    [SerializeField] private List<Transform> debugCubes;
     [SerializeField] private RectTransform dragBox;
+    private Vector2 startMousePos;
     private bool isDrag = false;
     public void DragSelectStart()
     {
         if (!isDrag)
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                startMousePos = Input.mousePosition;
+                dragBox.position = startMousePos;
+            }
+            
             if (Input.GetMouseButton(0))
             {
                 dragBox.gameObject.SetActive(true);
-                dragBox.position = Input.mousePosition;
+                //dragBox.position = Input.mousePosition;
+
+                float width = Mathf.Abs(startMousePos.x - Input.mousePosition.x);
+                float height = Mathf.Abs(startMousePos.y - Input.mousePosition.y);
+
+                float xhalf;
+                float yhalf;
+                
+                dragBox.sizeDelta = new Vector2(width,
+                    Mathf.Abs(height));
+
+                if (startMousePos.x - Input.mousePosition.x > 0)
+                {
+                    xhalf = width / 2;
+                }
+                else
+                {
+                    xhalf = width / 2 * -1;
+                }
+                
+                if (startMousePos.y - Input.mousePosition.y > 0)
+                {
+                    yhalf = height / 2;
+                }
+                else
+                {
+                    yhalf = height / 2 * -1;
+                }
+                
+                dragBox.position = Input.mousePosition + new Vector3(xhalf, yhalf);
             }
             else if (Input.GetMouseButtonUp(0))
             {
@@ -113,11 +154,35 @@ public class PlayerController : MonoBehaviour
 
     public void DragSelectEnd()
     {
-        dragBox.gameObject.SetActive(false);
+        Vector3 scrPos = dragBox.position + new Vector3(0, 0, camera.farClipPlane);
+        Vector3 centerPos = camera.ScreenToWorldPoint(scrPos);
 
-        dragBox.transform.position = 
+        float width = Mathf.Abs(startMousePos.x - Input.mousePosition.x);
+        float height = Mathf.Abs(startMousePos.y - Input.mousePosition.y);
+
+        if (Physics.Raycast(camera.transform.position, centerPos, out hit, Mathf.Infinity, worldMask))
+        {
+            foreach (var unit in unitList)
+            {
+                var unitScrPos = camera.WorldToScreenPoint(unit.transform.position);
+                Debug.Log(unitScrPos.x + " >= " + -width/2);
+                Debug.Log(unitScrPos.x + " <= " + width/2);
+                Debug.Log(unitScrPos.y + " <= " + height/2);
+                Debug.Log(unitScrPos.y + " >= " + -height/2);
+                if (unitScrPos.x >= dragBox.position.x + -width / 2 && unitScrPos.y >= dragBox.position.y + -height / 2 && unitScrPos.x <= dragBox.position.x + width / 2 &&
+                    unitScrPos.y <= dragBox.position.y + height / 2)
+                {
+                    unit.gameObject.SetActive(false);
+                }
+            }
+            
+        }
         
+        startMousePos = new Vector2(0,0);
+        dragBox.gameObject.SetActive(false);
         isDrag = false;
+        //if(Physics.Raycast())
+        Debug.Log("Drag End");
     }
     #endregion
     

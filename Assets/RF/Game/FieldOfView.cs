4ca3cbb5f90f6bc442903ea2d0f91b9b719cdf05
REFIRSER2 <84903837+REFIRSER2@users.noreply.github.com>
2021-11-23 18:55:01 +0900

    미니맵/전장의 안개 추가
    미니맵과 전장의 안개가 추가되었습니다.

--- Assets/RF/Game/FieldOfView.cs
+++ Assets/RF/Game/FieldOfView.cs
@@ -11,51 +11,27 @@ public class FieldOfView : MonoBehaviour
     #region 유니티 기본 내장 함수
     private void Start()
     {
-        SetupVision();
+
     }
 
     private void Update()
     {
-        for (int fx = 0; fx < visionGrid.width; fx++)
-        {
-            for (int fy = 0; fy < visionGrid.height; fy++)
-            {
-                visionGrid.grids[fx,fy] = 0;
-            }
-        }
-        
-
-        foreach (var unit in units)
-        {
-            int px, py;
-            px = Mathf.RoundToInt(unit.transform.position.x);
-            py = Mathf.RoundToInt(unit.transform.position.z);
-
-            for (int x = -Mathf.RoundToInt(unit.GetRange() / 100F / 2); x <= Mathf.RoundToInt(unit.GetRange() / 100F / 2); x++)
-            {
-                for (int y = -Mathf.RoundToInt(unit.GetRange() / 100F / 2); y <= Mathf.RoundToInt(unit.GetRange() / 100F / 2); y++)
-                {
-                    //Debug.Log(py);
-                    // px = 9, width = 10, height = 10
-                    // x = 0, x = 1, x = 2;
-                    if ((px + x >= 0 && px + x < visionGrid.width) && (py + y >= 0 && py + y < visionGrid.height))
-                    {
-                        visionGrid.grids[px + x, py + y] = 1;
-                        //Handles.DrawWireCube(new Vector3(px + x + 0.5F, 0, py + y + 0.5F), new Vector3(1F, 1F, 1F));
-                    }
-
-                    //colors[x + y * width] = 1;
-                }
-                
-               
-            }
-        }
-        
-        //fogTexture.SetPixels(colors);
+        UpdateFogCam();
     }
 
     #endregion
     
+    #region 카메라
+
+    [SerializeField] private Camera fogCamera;
+
+    private void UpdateFogCam()
+    {
+        fogCamera.transform.position = Camera.main.transform.position;
+    }
+    #endregion
+
+    /*
     #region FOV 적 찾기
     public VisionGrid visionGrid;
     [SerializeField] private int width, height;
@@ -74,14 +50,16 @@ public class FieldOfView : MonoBehaviour
 
         units = FindObjectOfType<PlayerController>().GetUnits();
     }
-    #endregion
+    #endregion*/
 }
 
+/*
 [CustomEditor(typeof(FieldOfView))]
 public class FieldOfViewEditor : Editor
 {
     private void OnSceneGUI()
     {
+        
        FieldOfView fov = target as FieldOfView;
        
        for (int x=0;x<fov.visionGrid.width; x++)
@@ -123,4 +101,4 @@ public struct VisionGrid
     public int players;
     public float range;
     public int[,] grids;
-}
+}*/
--- Assets/RF/Game/GameManager.cs
+++ Assets/RF/Game/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Steamworks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -15,17 +16,66 @@ public class GameManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(this.gameObject);
         }
-        else
-        {
-            Destroy(this.gameObject);
-        }
+    }
+
+    private void Start()
+    {
+        SetupTeam();
     }
 
     private void Update()
     {
-        
+        UpdateGame();
+    }
+    #endregion
+    
+    #region 팀
+    private SteamId[,] teamMembers = new SteamId[2, 4];
+
+    private void SetupTeam()
+    {
+        var index = 0;
+        foreach (var idStr in SteamManager.Instance.GetLobbyMemberIds())
+        {
+            ulong id = Convert.ToUInt64(idStr);
+            if (index < 4)
+            {
+                teamMembers[0, index] = id;
+            }
+            else
+            {
+                teamMembers[1, ] = id;
+            }
+            
+            index++;
+        }
+    }
+    #endregion
+    
+    #region 참가
+    
+    #endregion
+    
+    #region 게임 체크
+    private bool isEnd;
+    private void UpdateGame()
+    {
+        if (SteamManager.Instance.GetLobbyMemberIds().Count <= 1)
+        {
+            if (!isEnd)
+            {
+                EndGame();    
+            }
+        }
+    }
+    #endregion
+    
+    #region 게임 종료
+
+    private void EndGame()
+    {
+        isEnd = true;
     }
     #endregion
 }
